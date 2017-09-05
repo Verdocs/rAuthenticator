fdfa569c45879266d4ad2b15aeb863438ed0af01
AmirSaber Sharifi <amir.s.sharifi@gmail.com>
2017-09-05 14:32:25 -0400

    setup cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import * as express from 'express';
 import * as config from 'config';
 import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+
+const env = config.get<string>('env');
 
 const app = express();
+
+const corsSetting = cors({
+  origin: config.get<string>('cors.origin'),
+  credentials: config.get<string>('cors.credentials')
+});
+
+app.use(corsSetting);
 
 app.use(bodyParser.json());
 
