a496536414cc2ef21ae7f0dcc23e9b1e7c08bdbf
AmirSaber Sharifi <amir.s.sharifi@gmail.com>
2017-09-05 14:24:17 -0400

    add base

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello world');
+import * as express from 'express';
+import * as config from 'config';
+import * as bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/', (req, res) => {
+  res.send('hello');
+});
+
+const port = config.get<string>('port')
+app.listen(port, () => {
+  console.log(`Started listening on port ${port}`);
+});
